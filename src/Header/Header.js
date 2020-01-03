fd5ec99a0a833722a5073e280795facd455a3bad
unknown <egor0506070@mail.ru>
2020-01-03 22:59:45 +0300

    commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,20 +2,20 @@ import React from 'react'
 import s from './Header.module.scss'
 import menu from '../Charger_Customs_Free_Psd_Template-assets/Menu.png'
 import Logo from './Logo'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const Header = () => {
     return (
         <div className={s.Main}>
             <div className={s.Header}>
                 <span className={s.Icons}>
-               <Logo/>
-               </span>
+                    <Logo />
+                </span>
                 <div className={s.bar}>
                     <ul>
-                      <li>   <Link to='aboutus' smooth={true}>ABOUT US</Link></li>
-                        <li> <Link to='services' smooth={true}>SERVICES</Link></li>
-                        <li><Link to='projects' smooth={true}>PROJECTS</Link></li>
+                        <li><Link className={s.Link} to='aboutus' smooth={true}>ABOUT US</Link></li>
+                        <li><Link className={s.Link} to='services' smooth={true}>SERVICES</Link></li>
+                        <li><Link className={s.Link} to='projects' smooth={true}>PROJECTS</Link></li>
                     </ul>
                 </div>
                 <span className={s.Menu}>
@@ -29,8 +29,8 @@ const Header = () => {
                     Charger
              </div>
                 <div className={s.SecondText}>
-                One of the most powerful and recognized brand <br/>
-                 names in the custom automotive industry.
+                    One of the most powerful and recognized brand <br />
+                    names in the custom automotive industry.
                 </div>
                 <div className={s.More}>
                     <a href='#' className={s.ref}>READ MORE</a>
@@ -49,9 +49,9 @@ const Header = () => {
                 <div className={s.Numbers}>
                     <div className={s.first}>01</div>
                     <div className={s.between}></div>
-                     <div className={s.Second}>10</div>
-                     </div>
-                <div className={s.scroll}><Link to='footer' smooth={true}>Scroll down</Link></div>
+                    <div className={s.Second}>10</div>
+                </div>
+                <div className={s.scroll}><Link className={s.Link} to='footer' smooth={true}>Scroll down</Link></div>
             </div>
         </div>
     )
